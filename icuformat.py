3226adc89be21f60f97f01bb203428d81a631a0f
Aleksei Voronov <despawn@gmail.com>
2016-03-08 21:47:29 +0300

    Get formats dynamically (to be memoized)

--- icuformat.py
+++ icuformat.py
@@ -17,92 +17,94 @@ __all__ = ['Formatter']
 
 
 class Formatter(object):
-    def __init__(self, locale):
-        self.locale = Locale(locale)
-
-        self._short_date_format = DateFormat.createDateInstance(
-            DateFormat.SHORT, self.locale)
-        self._medium_date_format = DateFormat.createDateInstance(
-            DateFormat.MEDIUM, self.locale)
-        self._long_date_format = DateFormat.createDateInstance(
-            DateFormat.LONG, self.locale)
-
-        self._short_time_format = DateFormat.createTimeInstance(
-            DateFormat.SHORT, self.locale)
-        self._long_time_format = DateFormat.createTimeInstance(
-            DateFormat.LONG, self.locale)
-
-        self._short_datetime_format = DateFormat.createDateTimeInstance(
-            DateFormat.SHORT, DateFormat.SHORT, self.locale)
-        self._medium_datetime_format = DateFormat.createDateTimeInstance(
-            DateFormat.MEDIUM, DateFormat.SHORT, self.locale)
-        self._long_datetime_format = DateFormat.createDateTimeInstance(
-            DateFormat.LONG, DateFormat.LONG, self.locale)
-
-        hour_format = detect_hour_format(self.locale)
-        self._date_range_format = DateIntervalFormat.createInstance(
-            'yMMMMd', self.locale)
-        self._datetime_range_format = DateIntervalFormat.createInstance(
-            'yMMMMd%sm' % hour_format, self.locale)
-
-        self._number_format = NumberFormat.createInstance(self.locale)
-        self._spell_out_format = RuleBasedNumberFormat(
-            URBNFRuleSetTag.SPELLOUT, self.locale)
+    def __init__(self, locale_name):
+        self.locale = Locale(locale_name)
 
     # dates
 
     def short_date(self, d):
-        return self._short_date_format.format(force_datetime(d))
+        return self.get_date_format(DateFormat.SHORT).format(force_datetime(d))
 
     def medium_date(self, d):
-        return self._medium_date_format.format(force_datetime(d))
+        return self.get_date_format(DateFormat.MEDIUM).format(force_datetime(d))
 
     def long_date(self, d):
-        return self._long_date_format.format(force_datetime(d))
+        return self.get_date_format(DateFormat.LONG).format(force_datetime(d))
 
     # times
 
     def short_time(self, t):
-        return self._short_time_format.format(force_datetime(t))
+        return self.get_time_format(DateFormat.SHORT).format(force_datetime(t))
 
     def long_time(self, t):
-        return self._long_time_format.format(force_datetime(t))
+        return self.get_time_format(DateFormat.LONG).format(force_datetime(t))
 
     # datetimes
 
     def short_datetime(self, dt):
-        return self._short_datetime_format.format(dt)
+        return self.get_datetime_format(
+            DateFormat.SHORT, DateFormat.SHORT).format(dt)
 
     def medium_datetime(self, dt):
-        return self._medium_datetime_format.format(dt)
+        return self.get_datetime_format(
+            DateFormat.MEDIUM, DateFormat.SHORT).format(dt)
 
     def long_datetime(self, dt):
-        return self._long_datetime_format.format(dt)
+        return self.get_datetime_format(
+            DateFormat.LONG, DateFormat.LONG).format(dt)
 
     # date ranges
 
     def date_range(self, from_date, to_date):
         interval = DateInterval(force_datetime(from_date),
                                 force_datetime(to_date))
-        return self._date_range_format.format(interval)
+        return self.get_date_interval_format().format(interval)
 
     def datetime_range(self, from_dt, to_dt):
         interval = DateInterval(from_dt, to_dt)
-        return self._datetime_range_format.format(interval)
+        return self.get_datetime_interval_format().format(interval)
 
     # numbers
 
     def number(self, number):
-        return self._number_format.format(number)
+        return self.get_number_format().format(number)
 
     def spell_out(self, number):
-        return self._spell_out_format.format(number)
+        return self.get_number_format(URBNFRuleSetTag.SPELLOUT).format(number)
 
     def currency(self, amount, currency):
-        formatter = NumberFormat.createCurrencyInstance(
-            self.locale)
-        formatter.setCurrency(currency)
-        return formatter.format(amount)
+        return self.get_currency_format(currency).format(amount)
+
+    # formats
+
+    def get_date_format(self, style):
+        return DateFormat.createDateInstance(style, self.locale)
+
+    def get_time_format(self, style):
+        return DateFormat.createDateInstance(style, self.locale)
+
+    def get_datetime_format(self, date_style, time_style):
+        return DateFormat.createDateTimeInstance(
+            date_style, time_style, self.locale)
+
+    def get_date_interval_format(self):
+        return DateIntervalFormat.createInstance('yMMMMd', self.locale)
+
+    def get_datetime_interval_format(self):
+        hour_format = detect_hour_format(self.locale)
+        return DateIntervalFormat.createInstance(
+            'yMMMMd%sm' % hour_format, self.locale)
+
+    def get_number_format(self, rule_tag=None):
+        if rule_tag is None:
+            return NumberFormat.createInstance(self.locale)
+        else:
+            return RuleBasedNumberFormat(rule_tag, self.locale)
+
+    def get_currency_format(self, currency):
+        format_ = NumberFormat.createCurrencyInstance(self.locale)
+        format_.setCurrency(currency)
+        return format_
 
 
 def force_datetime(value):
